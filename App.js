18ff1429c047a450ec1e42be490728ce107400f3
anchisa27 <99910213+anchisa27@users.noreply.github.com>
2023-10-24 03:16:40 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MyNavigator from './component/MyNavigator';
+import Windetail from './screens/Windetail';
+
+
+
+export default function App() {
+  return (
+    <MyNavigator/>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
